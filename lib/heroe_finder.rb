317ae8fe4ecfa699ac96dc2b224ab9ca6020e623
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-07-18 12:28:13 -0500

    initial commit

---
+++ lib/heroe_finder.rb
@@ -0,0 +1,6 @@
+require "heroe_finder/version"
+
+module HeroeFinder
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/heroe_finder/version.rb
@@ -0,0 +1,3 @@
+module HeroeFinder
+  VERSION = "0.1.0"
+end
